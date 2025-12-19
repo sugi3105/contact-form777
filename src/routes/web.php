b56a96c4aff33e14a98eb80086bba2922c3826b4
sugi <tales10-orix51-saiko@outlook.jp>
2025-12-19 10:27:23 +0900

    ファイルの作成
    exit
    git commit -m ファイルの作成

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/confirm', [ContactController::class, 'confirm']);
+Route::post('/thanks', [ContactController::class, 'store']);
+Route::get('/admin', [ContactController::class, 'admin']);
 });
